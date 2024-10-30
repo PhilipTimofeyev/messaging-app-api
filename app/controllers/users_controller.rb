67aebc7a7fa57760e43ac0b0b298dfe1cd9feb10
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-10-29 18:06:05 -0700

    Render all users without current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_devise_api_token!, :set_current_user
   def index
-    @users = User.all
+    @users = User.where.not(id: @current_user.id)
 
     render json: @users
   end
 end
+
+
+
+  private
+
+  def set_current_user
+    current_user_id = current_devise_api_token[:resource_owner_id]
+    @current_user = User.find(current_user_id)
+  end
