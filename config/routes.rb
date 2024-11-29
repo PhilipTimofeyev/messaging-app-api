550e9dc0a35ebb5758462da3c2b9e35c4ccc3089
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-11-29 11:09:47 -0800

    Limit user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get "users/index"
   resources :messages, only: [ :create, :destroy ]
   resources :groups
   devise_for :users
-  resources :users
+  resources :users, only: [ :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
